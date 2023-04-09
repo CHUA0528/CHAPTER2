d8c6fd2a13670f2ef6a32f59429416bc9c794d33
CHUA0528 <choo27283@gmail.com>
2023-04-09 10:35:30 +0800

    new

--- newindex.js
+++ newindex.js
@@ -10,6 +10,81 @@ app.post('/bye', (req, res) => {
     res.send('bye bye World!')
   })
 
+//Define
+
+  let users = [
+    {
+        username: "soo",
+        password:"password",
+        name:"Ali",
+        email:"ali@student.utem.edu.my"
+    },
+    {
+        username:"CHUA",
+        password:"123456",
+        name:"CCF",
+        email:"choo27283@student.utem.edu.my"
+    }
+]
+
+//app.use(express.json())
+app.post('/',(req, res)=>{
+    let data =req.body
+    //res.send('Post request'+data.name)
+
+
+});
+
+
+app.post('/login',(req,res)=>{
+    let data =req.body;
+    res.send(
+      login(
+        data.username,
+        data.password
+      )
+    )
+});
+
+
+  
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+function login(username,password){
+  console.log("someone try to login with",username,password)
+  /*users.find(element=>{
+      console.log(element)
+
+  })*/
+
+  let matched=users.find(element=>element.username==username)
+  if(matched)
+  {
+      if(matched.password==password)
+      {
+          return matched
+      }
+      else
+      {
+          return "Password not matched"
+      }
+  }
+  else
+  {
+      return "Username not found"
+  }
+}
+
+app.post('/register',(req,res)=>{
+  let data =req.body
+  res.send(
+    register(
+      data.username,
+      data.password,
+      data.name,
+      data.email
+    )
+  );
+});
